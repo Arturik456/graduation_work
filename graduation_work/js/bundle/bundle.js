779c4255a3c6980607b20f2f505ceab2830247a1
Arturik456 <39768881+Arturik456@users.noreply.github.com>
2018-07-06 14:20:07 +0500

    Add files via upload

--- graduation_work/js/bundle/bundle.js
+++ graduation_work/js/bundle/bundle.js
@@ -1,29 +1,26 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 window.addEventListener('DOMContentLoaded', function() {
 
-	let modalEng = require('../parts/modalEng.js');
-	let modalPhone = require('../parts/modalPhone.js');
+	let modal = require('../parts/modal.js');
 	let calc = require('../parts/calc.js');
 	let tabs = require('../parts/tabs.js');
 	let tabsDec = require('../parts/tabsDec.js');
 	let ajax = require('../parts/ajax.js');
 	let img = require('../parts/img.js');
 	let time = require('../parts/time.js');
-	// let slider = require('../parts/slider.js');
 
-	modalEng();
-	modalPhone();
+
+	modal();
 	calc();
 	ajax();
 	tabs();
 	tabsDec();
 	img();
 	time('2018-07-7');
-	// slider();
 
 
 });
-},{"../parts/ajax.js":2,"../parts/calc.js":3,"../parts/img.js":4,"../parts/modalEng.js":5,"../parts/modalPhone.js":6,"../parts/tabs.js":7,"../parts/tabsDec.js":8,"../parts/time.js":9}],2:[function(require,module,exports){
+},{"../parts/ajax.js":2,"../parts/calc.js":3,"../parts/img.js":4,"../parts/modal.js":5,"../parts/tabs.js":6,"../parts/tabsDec.js":7,"../parts/time.js":8}],2:[function(require,module,exports){
 function ajax() {
 	
 let message = new Object();
@@ -80,206 +77,124 @@ module.exports = ajax;
 },{}],3:[function(require,module,exports){
 function calc() {
 	
- let buttonCalc = document.getElementsByClassName('glazing_price_btn'),
-  popupCalc = document.getElementsByClassName('popup_calc')[0],
-  popupCalcProfile = document.getElementsByClassName('popup_calc_profile')[0],
-  popupCalcProfileClose = document.getElementsByClassName('popup_calc_profile_close')[0],
-  popupCalcClose = document.getElementsByClassName('popup_calc_close')[0],
-  popupCalcButton = document.getElementsByClassName('popup_calc_button')[0],
-  inputWidth = document.getElementById('width'),
-  inputHeight = document.getElementById('height');
-
-  //объект
-  let data = {};
-
-  function checkInput(inp) {
-    inp.addEventListener('keypress', function(inp) {
-      let that = this;
-      setTimeout(function() {
-          let res = /[^0-999]/g.exec(that.value);
-          that.value = that.value.replace(res, '');
-      }, 0);
-    });
-  };
-
-  checkInput(inputWidth);
-  checkInput(inputHeight);
-      
-
-  for (let i = 0; i < buttonCalc.length; i++) {
-    buttonCalc[i].addEventListener('click', () => {
-      popupCalc.classList.add('show');
-      popupCalc.classList.add('animated');
-      popupCalc.classList.remove('fadeOut');
-      popupCalc.classList.add('fadeIn');
-    })
-  };
-  popupCalcClose.addEventListener('click', popupClose);
+ let popupCalcCont = document.querySelector('.popup_calc_content'),
+      balconIcons = popupCalcCont.querySelector('.balcon_icons'),
+      imgMini = balconIcons.getElementsByTagName('img'),
+      bigImg = popupCalcCont.querySelector('.big_img'),
+      imgBig = bigImg.getElementsByTagName('img'),
+      inputWidth = document.getElementById('width'),
+      inputHeight = document.getElementById('height'),
+      select = document.getElementById('view_type'),
+      checkbox = document.getElementsByClassName('checkbox'),
+      popCalcEnd = document.querySelector('.popup_calc_end'),
+      popupFormCalcEnd = popCalcEnd.querySelector('.popup_form');
 
 
-  function popupClose() {
-    popupCalc.classList.remove('fadeIn');
-    popupCalc.classList.add('fadeOut');
-    inputWidth.value = '';
-    inputHeight.value = '';
-    data = {};
-    setTimeout(() => {
-      popupCalc.classList.remove('show');
-      popupCalc.classList.add('hide');
-    }, 750);
-  }
-  // табы
-  function tabsCalc(event) {
-    let type = document.querySelectorAll('div.balcon_icons>a'),
-    typeImg  = document.querySelectorAll('div.balcon_icons>a>img'),
-    typeContent = document.querySelectorAll('div.big_img>img');
-
-    function hideTabContent(a) {
-      for (let i = a; i < typeContent.length; i++) {
-        typeContent[i].classList.remove('show');
-        typeContent[i].classList.add('hide');
-        typeContent[i].classList.add('animated');
-        typeContent[i].classList.add('fadeIn');
-        typeImg[i].classList.remove('scale');
-      };
-    };
-    hideTabContent(1);
-    
-    function showTabContent(b) {
-      if (typeContent[b].classList.contains('hide')) {
-        hideTabContent(0);
-        typeImg[b].classList.add('scale');
-        typeContent[b].classList.remove('hide');
-        typeContent[b].classList.add('show');
-      };
-    };
-
-    for (let i = 0; i < type.length; i++) {
-      type[i].addEventListener('click', (event) => {
-        event.preventDefault();
-        typeContent[i].setAttribute('value', i+1);
-        data.type = typeContent[i].getAttribute('value');
-        showTabContent(i);
-      })
-    };
-  }
-  tabsCalc();
-
-  setInterval(() => {
-    if (inputWidth.value == '' || inputHeight.value == '') {
-      popupCalcButton.setAttribute('disabled', 'true');
-     } else {
-      popupCalcButton.removeAttribute('disabled', 'true');
-     }
-  }, 0);
-
-  //кнопка Далее
-  popupCalcButton.addEventListener('click', () => {
-    data.width = inputWidth.value;
-    data.height = inputHeight.value;
-    popupCalcProfile.classList.remove('fadeOut');
-    popupCalc.classList.remove('show');
-    popupCalc.classList.add('hide');
-    popupCalcProfile.classList.add('show');
-    popupCalcProfile.classList.add('animated'); 
-  })
-
-  //profile
-  popupCalcProfileClose.addEventListener('click', popupProfileClose);
-
-  
-  function popupProfileClose() {
-    popupCalcProfile.classList.remove('fadeIn');
-    popupCalcProfile.classList.add('fadeOut');
-    inputWidth.value = '';
-    inputHeight.value = '';
-    for (let i = 0; i < checkbox.length; i++) {
-      checkbox[i].checked = false;
-    };
-    data = {};
-    setTimeout(() => {
-      popupCalcProfile.classList.remove('show');
-      popupCalcProfile.classList.add('hide');
-    }, 700);
-  }
-
-  let select = document.getElementById('view_type'),
-  buttonProfile = document.getElementsByClassName('popup_calc_profile_button')[0],
-  checkboxCustom = document.getElementsByClassName('checkbox-custom'),
-  checkbox = document.getElementsByClassName('checkbox'),
-  checkboxCold = document.getElementById('cold'),
-  checkboxWarm = document.getElementById('warm'),
-  popupCalcEnd = document.getElementsByClassName('popup_calc_end')[0];
-
-  function check() {
-    setInterval(() => {
-      if (checkbox[0].checked === false && checkbox[1].checked === false) {
-        buttonProfile.setAttribute('disabled', 'true');
-      } else {
-        buttonProfile.removeAttribute('disabled', 'true');
-      }
-      checkbox[0].addEventListener('click', function() {
-        checkbox[1].checked = false;
-        buttonProfile.removeAttribute('disabled', 'true');
-      })
-      checkbox[1].addEventListener('click', function() {
-        checkbox[0].checked = false;
-        buttonProfile.removeAttribute('disabled', 'true');
-      })
-    }, 0)
-    
-  };
-  check();
-
-  buttonProfile.addEventListener('click', () => {
-    data.category = select.options[select.selectedIndex].value;
-    for (let i = 0; i < checkbox.length; i++) {
-      if (checkbox[i].checked) {
-        data.checkbox = checkboxCustom[i].className.slice(16);
-      };
-    };
-    popupCalcProfile.classList.remove('show');
-    popupCalcProfile.classList.add('hide');
-    popupCalcEnd.classList.add('show');
-  })
-
-  //popupEnd
-  let popupCalcEndClose = document.getElementsByClassName('popup_calc_end_close')[0],
-  form = document.getElementsByClassName('form'),
-  inputName = document.querySelector('input.name'),
-  inputPhone = document.querySelector('input.phone'),
-  buttonCalcEnd = document.getElementsByClassName('btn-block')[8];
-
-  popupCalcEndClose.addEventListener('click', () => {
-     popupCalcEnd.classList.add('animated');
-     popupCalcEnd.classList.add('fadeOut');
-     inputWidth.value = '';
-     inputHeight.value = '';
-     for (let i = 0; i < checkbox.length; i++) {
-       checkbox[i].checked = false;
-     };
-     data = {};
-     setTimeout(() => {
-       popupCalcEnd.classList.remove('show');
-       popupCalcEnd.classList.add('hide');
-       popupCalcEnd.classList.remove('fadeOut');
-     }, 700)
-     
-  });
-
-  buttonCalcEnd.addEventListener('click', () => {
-    data.userName = inputName.value;
-    data.userPhone = inputPhone.value;
-    
-    //ajax
+  popupFormCalcEnd.addEventListener('submit', function(event) {
+    event.preventDefault();
+    //AJAX
     let request = new XMLHttpRequest();
-    request.open('POST', 'server.php');
-    request.setRequestHeader('Content-Type', 'aplication/x-www-form-urlencoded');
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'aplication/x-www-form-urlencoded');
 
     let formData = new FormData(data);
+    
+    console.log(formData);
 
     request.send(formData);
-  })
+
+
+
+    data.type = '';
+    data.width = 0;
+    data.height = 0;
+    data.cold = false;
+    data.warm = false;
+    inputWidth.value = '';
+    inputHeight.value = '';
+    checkbox[0].checked = false;
+    checkbox[1].checked = false;
+
+
+  });
+
+
+
+  select.addEventListener('change', () => {
+    data.type = select.value;
+    console.log(data);
+  });
+
+  inputWidth.addEventListener('keyup', () => {
+    inputWidth.value = inputWidth.value.replace(/[^\d]/g, '');
+    data.width = inputWidth.value;    
+    console.log(data);
+  });
+  inputHeight.addEventListener('keyup', () => {
+    inputHeight.value = inputHeight.value.replace(/[^\d]/g, '');
+    data.height = inputHeight.value;
+    console.log(data);  
+  });
+
+  function check(a,b) {
+    
+      if (checkbox[a].checked) {
+        checkbox[b].checked = false;
+      };
+
+      for (let i = 0; i < checkbox.length; i++) {
+        if (checkbox[0].checked) {
+          data.cold = true;          
+        } else {
+          data.cold = false;            
+        };
+        if (checkbox[1].checked) {
+          data.warm = true;          
+        } else {
+          data.warm = false;            
+          };
+      }
+      console.log(data);
+  };
+
+  checkbox[0].addEventListener('click', () => {
+    check(0,1);
+
+  });
+  checkbox[1].addEventListener('click', () => {
+    check(1,0);
+  });
+
+  const data = {
+    type : String,
+    width : 0,
+    height : 0,
+    cold : false,
+    warm : false
+  };
+  
+
+  popupCalcCont.addEventListener('click', (e) => {
+    e.preventDefault();
+    let target = e.target;
+    
+    for (let i = 0; i < imgMini.length; i++) {
+      if (target == imgMini[i]) {
+        console.log('равно ' + imgMini[i].src);
+        for (let t = 0; t < imgMini.length; t++) {
+          imgMini[t].style.width = '60px';          
+        }
+        imgMini[i].style.width = '150px';
+        for (let t = 0; t < imgBig.length; t++) {
+          imgBig[t].style.display = 'none';          
+        }
+        imgBig[i].style.display = 'inline-block';
+
+        break;
+      }
+      
+    }
+  });
 
 
 };
@@ -319,72 +234,118 @@ function img() {
 
 module.exports = img;
 },{}],5:[function(require,module,exports){
-function modalEng () {
+function modale() {
+let btnPopEng = document.getElementsByClassName('popup_engineer_btn')[0],
+      popEng = document.querySelector('.popup_engineer'),
+      popCalc = document.querySelector('.popup_calc'),
+      btnPopCalc = popCalc.querySelector('.popup_calc_button'),
+      popCalcProfile = document.querySelector('.popup_calc_profile'),
+      btnPopCalcProfile = popCalcProfile.querySelector('.popup_calc_profile_button'),
+      popCalcProfileClose = document.querySelector('.popup_calc_profile_close'),
+      popCalcEnd = document.querySelector('.popup_calc_end'),
+      popCalcEndClose = popCalcEnd.querySelector('.popup_calc_end_close'),
+      popEngForm = popEng.querySelector('.form'),
+      popup = document.querySelector('.popup'),
+      popupClose = document.querySelectorAll('.popup_close'),
+      popupCalcClose = document.querySelector('.popup_calc_close'),
+      phoneLink = document.querySelectorAll('.phone_link'),
+      btnCalc = document.querySelectorAll('.popup_calc_btn');
 
-	let btnEng = document.querySelector('.header_btn'),
-		window = document.querySelector('.popup_engineer'),
-		contentEng = document.getElementsByClassName('popup_content')[1];
+  console.log(btnPopCalc);
+    
+  btnPopCalc.addEventListener('click', function () {
+    console.log(this);
+    modalClose(popCalc);
+    modalOpen(popCalcProfile);
+  });
 
-		btnEng.addEventListener('click', function() {
-			window.style.display = 'block';
-		});
+  btnPopCalcProfile.addEventListener('click', function () {
+    console.log(this);
+    modalClose(popCalcProfile);
+    modalOpen(popCalcEnd);
+  });
+  
 
-		window.addEventListener('click', function(event) {
-			if(event.target == contentEng 
-				|| event.target == document.getElementsByClassName('popup_dialog')[1]
-				|| event.target == document.getElementsByClassName('popup_form')[1]
-				|| event.target == document.getElementsByClassName('form')[1]
-				|| event.target == document.getElementsByClassName('form-control')[14]
-				|| event.target == document.getElementsByClassName('form-control')[15]) {
-				window.style.display = 'block';
-			} else {
-				window.style.display = "none";
-			}
-			
-		}); 
+  function modalClose(pop) {
+    pop.style.display = '';
+  }
+
+  function modalOpen(pop) {
+    pop.style.display = 'block';
+  }
+
+  btnPopEng.addEventListener('click', function () {
+    console.log(this);
+    modalOpen(popEng);
+    modalOpen(popup);
+  });
+  
+  phoneLink[0].addEventListener('click', function () {
+    modalOpen(popup);
+  });
+  phoneLink[1].addEventListener('click', function () {
+    event.preventDefault();
+    modalOpen(popup);
+  });
+  popupClose[0].addEventListener('click', function (event) {
+    
+    modalClose(popup);
+    
+  });
+
+  popupCalcClose.addEventListener('click', function (event) {
+    
+    modalClose(popCalc);
+    
+  });
+
+  popCalcProfileClose.addEventListener('click', function (event) {
+    
+    modalClose(popCalcProfile);
+    
+  });
+
+  popCalcEndClose.addEventListener('click', function (event) {
+    
+    modalClose(popCalcEnd);
+    
+  });
+  
+  //
+  popupClose[1].addEventListener('click', function (event) {
+    
+    modalClose(popEng);
+    modalClose(popup);
+    
+  });
+  document.addEventListener('click', (e) => {
+
+    if (e.target == popEng) {
+      modalClose(popEng);
+      modalClose(popup);
+    }
+
+
+    if (e.target == popup) {
+      modalClose(popup);
+    }
+
+    if (e.target.classList.contains('popup_calc_btn')) {
+
+      modalOpen(popCalc);
+    }
+
+
+  });
+
+  setTimeout(() => {
+    modalOpen(popup);
+  }, 60000);
+  
 
 };
-
-module.exports = modalEng;
-
+module.exports = modale;
 },{}],6:[function(require,module,exports){
-function modalPhone() {
-	let phone = document.getElementsByClassName('phone_link')[0],
-		phoneFutter = document.getElementsByClassName('phone_link')[1],
-		popup = document.querySelector('.popup'),
-		content = document.getElementsByClassName('popup_content')[0];
-
-	phone.addEventListener('click', function() {
-		popup.style.display = "block";
-	});
-
-	phoneFutter.addEventListener('click', function() {
-		popup.style.display = "block";
-	});
-	
-
-	popup.addEventListener('click', function(event) {
-		if(event.target == content 
-			|| event.target == document.getElementsByClassName('popup_dialog')[0]
-				|| event.target == document.getElementsByClassName('popup_form')[0]
-				|| event.target == document.getElementsByClassName('form')[0]
-				|| event.target == document.getElementsByClassName('form-control')[12]
-				|| event.target == document.getElementsByClassName('form-control')[13]) {
-	 		popup.style.display = 'block';
-	 	} else {
-	 		popup.style.display = 'none';
-	 	}
-	});
-
-	setTimeout(function() {
-		popup.style.display = "block";
-	}, 60000);
-
-
-
-};
-module.exports = modalPhone;
-},{}],7:[function(require,module,exports){
 function tabs() {
 	let tab = document.getElementsByClassName('glazing_block'),
 		glazing = document.getElementsByClassName('glazing')[0],
@@ -421,7 +382,7 @@ module.exports = tabs;
 
 
 
-},{}],8:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 function tabsDec()  {
 let noClick = document.querySelectorAll('.no_click'),
 	decor = document.querySelector('.decoration'),
@@ -452,7 +413,7 @@ let noClick = document.querySelectorAll('.no_click'),
   });
  }
 module.exports = tabsDec;
-},{}],9:[function(require,module,exports){
+},{}],8:[function(require,module,exports){
 function timer(deadLine) {
 
 	let eTimer = document.getElementsByClassName('eTimer')[0];
--- graduation_work/js/bundle/script.js
+++ graduation_work/js/bundle/script.js
@@ -1,24 +1,21 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-	let modalEng = require('../parts/modalEng.js');
-	let modalPhone = require('../parts/modalPhone.js');
+	let modal = require('../parts/modal.js');
 	let calc = require('../parts/calc.js');
 	let tabs = require('../parts/tabs.js');
 	let tabsDec = require('../parts/tabsDec.js');
 	let ajax = require('../parts/ajax.js');
 	let img = require('../parts/img.js');
 	let time = require('../parts/time.js');
-	// let slider = require('../parts/slider.js');
 
-	modalEng();
-	modalPhone();
+
+	modal();
 	calc();
 	ajax();
 	tabs();
 	tabsDec();
 	img();
 	time('2018-07-7');
-	// slider();
 
 
 });
